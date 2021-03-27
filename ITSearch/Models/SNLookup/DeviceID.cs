3b78800af7d37986ace68484e32c409aa605097d
Kyle Supple <38538913+kysu1313@users.noreply.github.com>
2021-03-26 20:04:47 -0400

    polishing ifixit interfaces and adding sn db

---
+++ ITSearch/Models/SNLookup/DeviceID.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ITSearch.Models.SNLookup
+{
+    public class DeviceID
+    {
+        private const string URL = "https://afit-deviceidentifier-v1.p.rapidapi.com/v1/apple-serials/";
+        private const string APPLE_URL = "https://support-sp.apple.com/sp/product?cc=";
+        private string UrlParams = "";
+
+
+        //  F2MVLA7KJCLM
+
+        public DeviceID() { }
+
+        /**
+         * Call DeviceID to get info about a SN.
+         * I need to find a different solution. This caps at 5 calls / day...
+         */
+        public SNLookupResponse CheckID(string id)
+        {
+            try
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    webClient.Headers.Add("Content-Type", "application/json; charset=utf-8"); //; charset=utf-8
+                    webClient.Headers.Add("x-rapidapi-key", "a22bbde417msh8b69cc457d8670ep1a8c50jsn92c6d15e5173"); 
+                    webClient.Headers.Add("x-rapidapi-host", "afit-deviceidentifier-v1.p.rapidapi.com");
+                    //webClient.Headers.Add("useQueryString", "true");
+                    webClient.BaseAddress = URL;
+                    var json = webClient.DownloadString(id + "/");
+                    System.Diagnostics.Debug.WriteLine(json);
+                    JObject job = JObject.Parse(json);
+
+                    return ParseGuideJson(job);
+                }
+            }
+            catch (WebException ex)
+            {
+                throw;
+            }
+        }
+
+        public void AppleSnLookup(string sn)
+        {
+            // This isn't the best method either.
+            // Takes the last 4 of the sn and check against apples website. 
+            // Problem is that it's not 100% consistent. SN schemes have changed over the years.
+
+            try
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    webClient.Headers.Add("Content-Type", "application/json; charset=utf-8"); //; charset=utf-8
+                    webClient.Headers.Add("x-rapidapi-key", "a22bbde417msh8b69cc457d8670ep1a8c50jsn92c6d15e5173");
+                    webClient.Headers.Add("x-rapidapi-host", "afit-deviceidentifier-v1.p.rapidapi.com");
+                    //webClient.Headers.Add("useQueryString", "true");
+                    webClient.BaseAddress = URL;
+                    var json = webClient.DownloadString(sn + "/");
+                    System.Diagnostics.Debug.WriteLine(json);
+                    JObject job = JObject.Parse(json);
+
+                    //return ParseGuideJson(job);
+                }
+            }
+            catch (WebException ex)
+            {
+                throw;
+            }
+
+        }
+
+        public void PopulateSnDb()
+        {
+
+        }
+
+        private SNLookupResponse ParseGuideJson(JObject job)
+        {
+            // Prevent null values in response
+            var settings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                MissingMemberHandling = MissingMemberHandling.Ignore
+            };
+
+            SNLookupResponse ri;
+
+            try
+            {
+                ri = JsonConvert.DeserializeObject<SNLookupResponse>(job.ToString(), settings);
+            }
+            catch (NullReferenceException ex)
+            {
+                throw;
+            }
+
+            return ri;
+        }
+    }
+}
---
+++ ITSearch/Models/SNLookup/SNLookupResponse.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITSearch.Models.SNLookup
+{
+
+    public class SNLookupResponse
+    {
+        public string anonymised { get; set; }
+        public Configurationcode configurationCode { get; set; }
+        public string coverageUrl { get; set; }
+        public string id { get; set; }
+        public Manufacturing manufacturing { get; set; }
+        public string serialType { get; set; }
+        public Uniqueid uniqueId { get; set; }
+    }
+
+    public class Configurationcode
+    {
+        public Appleinternalnameclass appleInternalNameClass { get; set; }
+        public string code { get; set; }
+        public object colour { get; set; }
+        public Image image { get; set; }
+        public object ram { get; set; }
+        public string skuHint { get; set; }
+        public object storage { get; set; }
+    }
+
+    public class Appleinternalnameclass
+    {
+        public string id { get; set; }
+    }
+
+    public class Image
+    {
+        public int height { get; set; }
+        public string url { get; set; }
+        public int width { get; set; }
+    }
+
+    public class Manufacturing
+    {
+        public string city { get; set; }
+        public string company { get; set; }
+        public string country { get; set; }
+        public string date { get; set; }
+        public string flag { get; set; }
+        public string id { get; set; }
+    }
+
+    public class Uniqueid
+    {
+        public int productionNo { get; set; }
+        public string value { get; set; }
+    }
+
+}
