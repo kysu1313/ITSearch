8846b18ba44f4b71a9281e551db92278c4f3f356
Kyle Supple <38538913+kysu1313@users.noreply.github.com>
2021-03-28 18:33:40 -0400

    tts

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -13,7 +13,7 @@ namespace ITSearch.Data
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+            : base(options) 
         {
         }
 
